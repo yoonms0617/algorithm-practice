8edc6b0510bc2cb4147a4b457811baa076ce8ee7
YoonMinsoo <102194801+yoonms0617@users.noreply.github.com>
2022-03-24 08:47:58 +0900

    Add 직사각형 별찍기

---
+++ programmers/level1/직사각형 별찍기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
