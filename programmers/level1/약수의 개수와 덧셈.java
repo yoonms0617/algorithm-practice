9fa965b62e6bcd25f8a2c521faf98393b60cafad
YoonMinsoo <102194801+yoonms0617@users.noreply.github.com>
2022-03-27 11:42:30 +0900

    Add 약수의 개수와 덧셈

---
+++ programmers/level1/약수의 개수와 덧셈.java
@@ -0,0 +1,22 @@
+class Solution {
+  
+    public int solution(int left, int right) {
+        int answer = 0;
+        int cnt = 0;
+        for (int i = left; i <= right; i++) {
+            cnt = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j ==0) {
+                    cnt++;
+                }
+            }
+            if (cnt % 2 == 0) {
+                answer = answer + i;
+            } else {
+                answer = answer - i;
+            }
+        }
+        return answer;
+    }
+  
+}
