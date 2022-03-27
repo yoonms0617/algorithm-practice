8ae569f6f3382da5613566900119ee16baa41009
YoonMinsoo <102194801+yoonms0617@users.noreply.github.com>
2022-03-27 11:30:11 +0900

    Add 음양 더하기

---
+++ programmers/level1/음양 더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+  
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int num = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            num = absolutes[i];
+            if (signs[i]) {
+                answer = answer + num;
+            } else {
+                answer = answer - num;
+            }
+        }
+        return answer;
+    }
+  
+}
