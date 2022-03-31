6c7fd3e3b73c1789e4df5b6a7e1c6e8c12dfabb4
YoonMinsoo <yoonminwater@gmail.com>
2022-03-31 19:40:10 +0900

    Add 자릿수 더하기

---
+++ programmers/level1/자릿수 더하기.java
@@ -0,0 +1,12 @@
+public class Solution {
+  
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer = answer + (n % 10);
+            n = n / 10;
+        }
+        return answer;
+    }
+  
+}
