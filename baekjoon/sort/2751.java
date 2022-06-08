c657573843715807dae25e44867d0ed55cbc2ee8
YoonMinsoo <yoonminwater@gmail.com>
2022-06-08 22:45:31 +0900

    solve: 수 정렬하기 2

---
+++ baekjoon/sort/2751.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        for (int value : list) {
+            sb.append(value).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+}
