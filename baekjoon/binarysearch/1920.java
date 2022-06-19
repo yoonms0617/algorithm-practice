de5feba122865b9d2f61c0db5d52596a09aa7b6e
YoonMinsoo <yoonminwater@gmail.com>
2022-06-19 14:01:02 +0900

    solve: 수 찾기

---
+++ baekjoon/binarysearch/1920.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] list = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(list[i]);
+        }
+        Arrays.sort(arr);
+        n = Integer.parseInt(br.readLine());
+        list = br.readLine().split(" ");
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i = 0; i < n; i++) {
+            int target = Integer.parseInt(list[i]);
+            int res = binarySearch(arr, target, start, end);
+            bw.write(String.valueOf(res));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
